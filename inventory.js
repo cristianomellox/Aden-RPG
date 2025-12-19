00206099bc44f29bfc82e9c1340bbed52a09e2b6
Cristiano Mello Varella <eusouocris.84@gmail.com>
2025-12-18 22:24:32 -0300

    Add files via upload

--- inventory.js
+++ inventory.js
@@ -15,7 +15,7 @@ let selectedItem = null;
 const DB_NAME = "aden_inventory_db";
 const STORE_NAME = "inventory_store";
 const META_STORE = "meta_store";
-const DB_VERSION = 26; // Incrementado para limpar caches antigos e evitar conflitos
+const DB_VERSION = 27; // Incrementado para limpar caches antigos e evitar conflitos
 
 function openDB() {
     return new Promise((resolve, reject) => {

1889bb1fe8e809adf4abe55c1b1b10f2fbe18038
Cristiano Mello Varella <eusouocris.84@gmail.com>
2025-12-22 01:03:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1384,7 +1384,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       
       if (key === "bolsaModal") {
-        window.location.href = "/inventory.html";
+        window.location.href = "go:bag";
         return;
       }
       if (key === "pvModal") {

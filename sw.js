f614e5f4c8c08c80d005bdee549eeb74918f9cd7
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-01-03 23:04:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // AUMENTE ESTE NÚMERO SEMPRE QUE FIZER UMA ATUALIZAÇÃO NO JOGO (V1 -> V2 -> V3...)
 // Se você não mudar isso, os jogadores continuarão vendo a versão antiga!
-const CACHE_NAME = "aden-static-v4";
+const CACHE_NAME = "aden-static-v5";
 
 // Arquivos vitais para o jogo abrir (App Shell)
 const STATIC_FILES = [

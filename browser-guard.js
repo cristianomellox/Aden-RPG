e29580e63a0ddcfbd6baf4bd94709bf33a6c3e8b
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-04-24 01:14:47 -0300

    Add files via upload

--- browser-guard.js
+++ browser-guard.js
@@ -407,7 +407,7 @@
 
     const lbl = document.createElement('span');
     lbl.textContent = label;
-    lbl.style.cssText = 'font-size:1.1em; color:gold; text-align:center; line-height:1.2;';
+    lbl.style.cssText = 'font-size:1.3em; color:gold; text-align:center; line-height:1.2;';
 
     card.appendChild(img);
     card.appendChild(lbl);

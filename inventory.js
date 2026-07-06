4429fd80d248e13405ca6b443b9687d8233a4002
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-07-06 15:29:25 -0300

    Add files via upload

--- inventory.js
+++ inventory.js
@@ -1252,7 +1252,7 @@ async function showItemDetails(item) {
                 const formattedName = formatAttrName(item.reforge_slot1.attr);
                 let formattedValue = item.reforge_slot1.value;
                 if (['TAXA CRIT','DANO CRIT','EVASÃO'].includes(formattedName)) formattedValue += '%';
-                const textStyle1 = `font-size:1.3em; margin:0; color:silver; font-weight:bold; text-shadow: none;
+                const textStyle1 = `font-size:1.1em; margin:0; color:silver; font-weight:bold; text-shadow: none;
   background: linear-gradient(to bottom, lightblue 0%, white 50%, silver 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;`;
@@ -1273,7 +1273,7 @@ async function showItemDetails(item) {
                 const formattedName = formatAttrName(item.reforge_slot2.attr);
                 let formattedValue = item.reforge_slot2.value;
                 if (['TAXA CRIT','DANO CRIT','EVASÃO'].includes(formattedName)) formattedValue += '%';
-                const textStyle2 = `font-size:1.3em; margin:0; color:silver; font-weight:bold; text-shadow: none;
+                const textStyle2 = `font-size:1.1em; margin:0; color:silver; font-weight:bold; text-shadow: none;
   background: linear-gradient(to bottom, lightblue 0%, white 50%, silver 100%);
   background: linear-gradient(to bottom, lightblue 0%, white 50%, silver 100%);
   background: linear-gradient(to bottom, lightblue 0%, white 50%, silver 100%);

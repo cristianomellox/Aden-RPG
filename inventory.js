a6f7a6608921d5cee8ad0f9973ec5484fb65c690
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-07-06 21:12:04 -0300

    Add files via upload

--- inventory.js
+++ inventory.js
@@ -1253,10 +1253,9 @@ async function showItemDetails(item) {
                 let formattedValue = item.reforge_slot1.value;
                 if (['TAXA CRIT','DANO CRIT','EVASÃO'].includes(formattedName)) formattedValue += '%';
                 const textStyle1 = `font-size:1.1em; margin:0; color:silver; font-weight:bold; text-shadow: none;
-  background: linear-gradient(to bottom, black 0%, white 30%, blue 100%);
+  background: linear-gradient(to bottom, black 20%, white 30%, blue 100%);
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-text-stroke: 0.6px black;`;
+  -webkit-text-fill-color: transparent;`;
                 refineRow1.innerHTML = `<img src="https://aden-rpg.pages.dev/assets/refund.webp" class="refine-icon" style="width:38px;height:38px;"><p style="${textStyle1}">${formattedName} +${formattedValue}</p>`;
                 refineRow1.style.setProperty('background', rarityGradient(item.reforge_slot1.color));
                 refineRow1.style.height = "15px";

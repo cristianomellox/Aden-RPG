64e12f672270685284fff2762d18aec46e28dc23
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-09-16 17:12:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4345,7 +4345,7 @@ function enableMapInteraction() {
     let currentX = 0, currentY = 0;
     let currentScale = 1;
     let MIN_SCALE = 0.35; // recalculado dinamicamente para preencher a tela
-    const MAX_SCALE = 0.5;  // limite máximo de zoom-in
+    const MAX_SCALE = 0.8;  // limite máximo de zoom-in
 
     // ── Inércia ─────────────────────────────────────────────────────────────
     let velocityX = 0, velocityY = 0;

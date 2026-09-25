7d933cff1bb598fa3f424caf28ef568bf13e7529
Cristiano Mello Varella <eusouocris.84@gmail.com>
2026-09-24 21:45:50 -0300

    Add files via upload

--- razar.js
+++ razar.js
@@ -1158,9 +1158,9 @@ const MOB_WALK_SHEETS = {
   
   aranha_artica: { 
       down: { 
-        cols: 6, rows: 6, frames: 36, scale: 1.1 }, up: { 
-          cols: 6, rows: 6, frames: 36, scale: 1.1 }, side: { 
-            cols: 6, rows: 6, frames: 36, scale: 1.1 } },
+        cols: 6, rows: 6, frames: 36, scale: 1.0 }, up: { 
+          cols: 6, rows: 6, frames: 36, scale: 1.0 }, side: { 
+            cols: 6, rows: 6, frames: 36, scale: 1.0 } },
             
             yeti: { 
       down: { 
@@ -1170,15 +1170,15 @@ const MOB_WALK_SHEETS = {
             
             fenrir_montanhes: { 
       down: { 
-        cols: 6, rows: 6, frames: 36, scale: 1.3 }, up: { 
-          cols: 6, rows: 6, frames: 36, scale: 1.1 }, side: { 
-            cols: 6, rows: 6, frames: 36, scale: 1.3 } },
+        cols: 6, rows: 6, frames: 36, scale: 1.2 }, up: { 
+          cols: 6, rows: 6, frames: 36, scale: 1.2 }, side: { 
+            cols: 6, rows: 6, frames: 36, scale: 1.1 } },
             
             golem_de_gelo: { 
       down: { 
-        cols: 6, rows: 6, frames: 36, scale: 1.1 }, up: { 
-          cols: 6, rows: 6, frames: 36, scale: 1.1 }, side: { 
-            cols: 6, rows: 6, frames: 36, scale: 1.1 } },
+        cols: 6, rows: 6, frames: 36, scale: 1.2 }, up: { 
+          cols: 6, rows: 6, frames: 36, scale: 1.2 }, side: { 
+            cols: 6, rows: 6, frames: 36, scale: 1.2 } },
   
 };
 
